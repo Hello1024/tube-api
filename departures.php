1be0a822f142b790526e3d3e786d841dae92f62c
Hello1024 <omattos@gmail.com>
2019-05-14 21:30:47 +0100

    reorder tracks

--- departures.php
+++ departures.php
@@ -28,17 +28,34 @@ $line_colours = array(//"B" => "#AB4800",
                       "W" => "#5CADAD"
                       );
 
+$eb_map = array("TPAECM" => 1000,
+                "TPAABECM" => 900,
+                "TPBBECM" => 800,
+                "TPCECM" => 700,
+                "TPDECM" => 600,
+                "TPFECM" => 500
+                );
+
+$wb_map = array("558",
+                "O",
+                "P");
+
+
 
 ?>
 <center>
 <br />
 <table class="table table-condensed" style="width:100%; font-family: 'VT323'; color:#FFBF00; font-size: 30px; font-weight: 400; ">
 <?php
-date_default_timezone_set("Europe/London");
+date_default_timezone_set("Etc/GMT");
+
+
+$wbdepartures = array();
+$ebdepartures = array();
+
 foreach($lines as $line){
     $url = 'http://cloud.tfl.gov.uk/TrackerNet/PredictionDetailed/'.$line.'/'.$station;
     $xml_str = file_get_contents($url);
-    $file = "logfile.csv";
     $logentry = time().",";
     try {
         $xmml = new SimpleXMLElement($xml_str);
@@ -46,23 +63,60 @@ foreach($lines as $line){
         $platforms_rev = array_reverse((array)$platforms);
         foreach(array(1,0) as $platformno) {
           $platform = $platforms[$platformno];
-          echo "<tr><td id=\"platform\">", $platform['N'], "<br /></td></tr>";
-          $traincounter = 0;
           foreach($platform -> T as $train) {
-              $logentry .= strtotime($train['DepartTime']) . "," . $train['TrackCode'] . "," . $train['TripNo'] . $train['SetNo'] . $train['LCID'] . ",";
-              $lastseen = time() - strtotime($train['DepartTime']);
-              $timetoestimate = $train['SecondsTo'] - $lastseen;
-              if ($traincounter<5)
-                echo "<tr><td id=\"departtime\"><strong>", $train['Destination'], "</strong></td> <td><div class='time'>",$lastseen>120?("Stuck ".$train['TimeTo']." away"):$timetoestimate,  "</div></td> <td style='width: 0%'>","</td></tr>";
-              $traincounter++;
+              if($platformno == 0){
+                    array_push($wbdepartures, $train);
+              } else {
+                    array_push($ebdepartures, $train);
+                     }
           }
         }
-        file_put_contents($file, $logentry . "\n", FILE_APPEND | LOCK_EX);
     } catch (Exception $e) {
         //echo $e, $line;
     }
 
 }
+
+//Sort
+
+function build_sorter($key) {
+    return function ($a, $b) use ($key) {
+        return strnatcmp($a[$key], $b[$key]);
+    };
+}
+
+usort($ebdepartures, build_sorter("TimeTo"));
+usort($wbdepartures, build_sorter("TimeTo"));
+
+//Display
+echo "<tr><td id=\"platform\">", "Platform 1 - Eastbound", "<br /></td></tr>";
+//echo "<tr><td style='width: 0%'><img src=\"img/eb-track.jpg\" /></td></tr>";
+
+//echo "<tr><td><img STYLE=\"position:absolute; LEFT:150px;\"  src=\"img/DistTrainEB.jpg\" /></td></tr>";
+//<img src=\"img/PiccTrainEB.jpg\" />
+
+$traincounter = 0;
+foreach($ebdepartures as $train) {
+    $lastseen = time() - strtotime($train['DepartTime']);
+    $timetoestimate = $train['SecondsTo'] - $lastseen;
+    if ($traincounter<5)
+        echo "<tr><td id=\"departtime\"><strong>", $train['Destination'], "</strong></td> <td><div class='time'>",$lastseen>120?("Stuck ".$train['TimeTo']." away"):$timetoestimate,  "</div></td> <td style='width: 0%'>","</td></tr>";
+    $traincounter++;
+    }
+    
+
+
+
+
+echo "<tr><td id=\"platform\">", "Platform 2 - Westbound", "<br /></td></tr>";
+$traincounter = 0;
+foreach($wbdepartures as $train) {
+    $lastseen = time() - strtotime($train['DepartTime']);
+    $timetoestimate = $train['SecondsTo'] - $lastseen;
+    if ($traincounter<5)
+        echo "<tr><td id=\"departtime\"><strong>", $train['Destination'], "</strong></td> <td><div class='time'>",$lastseen>120?("Stuck ".$train['TimeTo']." away"):$timetoestimate,  "</div></td> <td style='width: 0%'>","</td></tr>";
+    $traincounter++;
+    }
 
 ?>
 </table>
